1af7092fc24ce7963034a9235fc5f34799e6ae84
djkoogy <djkoogy@gmail.com>
2025-06-02 04:15:42 +0000

    상대주소

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from folium.plugins import MarkerCluster
 
 st.title("진주시 CCTV 현황")
 
-df = pd.read_csv("/workspaces/Test-20250526/jinju_cctv_20250513.csv", encoding='euc-kr')
+df = pd.read_csv("jinju_cctv_20250513.csv", encoding='euc-kr')
 
 st.dataframe(df, height=200)
 
